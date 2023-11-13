1cbf43115909ecf558588e15bd05e588beecc7a2
Uriah Blatherwick <uriahb@gmail.com>
2023-11-13 13:50:08 -0600

    Initial cart summary

---
+++ cart-summary/src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+  import CartSummary from './CartSummary'
+</script>
+
+<CartSummary />
---
+++ cart-summary/src/CartSummary.svelte
@@ -0,0 +1,51 @@
+<script>
+  let count = 0;
+  let items = [];
+  let categories = {};
+
+  let updateCategories = () => {
+    categories = {};
+    items.forEach(item => {
+      if(!categories[item.category]){
+        categories[item.category] = 0;
+      }
+      categories[item.category] += item.value;
+    });
+  }
+  let addToCartHandler = (event) =>{
+    items.push(event.detail);
+    
+    count = items.length;
+    updateCategories();
+  }
+
+  let clearCartHandler = () => {
+    count = 0;
+    items = [];
+    categories = {};
+  }
+
+  window.addEventListener('addToCart', addToCartHandler);
+  window.addEventListener('clearCart', clearCartHandler);
+</script>
+<div class="container">
+      <p class="d-inline-flex gap-1">
+          <i class="bi bi-cart"></i>Cart ({count})
+          <a class="btn btn-primary" data-bs-toggle="collapse" href="#cartSummary" role="button" aria-expanded="false" aria-controls="cartSummary">
+              Show Summary
+          </a>
+      </p>
+      <div class="collapse" id="cartSummary">
+          <div class="card card-body">
+            {#each Object.entries(categories) as [key, value], i}
+            {key} - ${value}
+            {/each}
+          </div>
+      </div>
+</div>
+
+<svelte:head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+</svelte:head>
