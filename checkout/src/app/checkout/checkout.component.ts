a8431a1925123ea860042ceaef68b31de6abe708
Kyle Merritt <kyle.merritt@nbcuni.com>
2023-11-13 13:47:39 -0500

    added count and + - buttons

--- checkout/src/app/checkout/checkout.component.ts
+++ checkout/src/app/checkout/checkout.component.ts
@@ -1,44 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Product } from '../models/product';
+import { CartItem } from '../models/cart-item';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent implements OnInit {
+export class CheckoutComponent implements OnInit, OnChanges {
   total: number = 0;
   totalItems: number = 0;
-  products: Product[] = [
+  items: CartItem[] = [
     {
       "id": "0",
       "name": "Laptop",
       "price": 999.99,
-      "category": "Electronics"
+      "category": "Electronics",
+      "count": 1,
     },
     {
       "id": "1",
       "name": "T-shirt",
       "price": 19.99,
-      "category": "Apparel"
+      "category": "Apparel",
+      "count": 1,
     },
     {
       "id": "2",
       "name": "Headphones",
       "price": 79.99,
-      "category": "Electronics"
+      "category": "Electronics",
+      "count": 1,
     },
     {
       "id": "3",
       "name": "Running Shoes",
       "price": 49.99,
-      "category": "Footwear"
+      "category": "Footwear",
+      "count": 1,
     },
     {
       "id": "4",
       "name": "Backpack",
       "price": 39.99,
-      "category": "Accessories"
+      "category": "Accessories",
+      "count": 1,
     }
   ];
   
@@ -46,10 +52,16 @@ export class CheckoutComponent implements OnInit {
 
   ngOnInit(): void {
     //TODO set up event listener for add item event
-    this.products.forEach(product => {
+    this.items.forEach(product => {
       this.total = this.total + product.price;
     });
-    this.totalItems = this.products.length;
+    this.totalItems = this.items.reduce((totalItems, item) => {
+      return totalItems += item.count
+    }, 0);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
   }
 
   buy(): void {
@@ -57,19 +69,37 @@ export class CheckoutComponent implements OnInit {
   }
 
   cleanCart() {
-    this.products = [];
+    this.items = [];
     this.totalItems = 0;
     this.total = 0;
 
     //TODO Send clear cart event
   }
 
-  deleteItem(deletedProduct: Product) {
-    this.products = this.products.filter(product => {
-      return product.id !== deletedProduct.id; 
+  deleteItem(deletedCartItem: CartItem) {
+    this.items = this.items.filter(item => {
+      return item.id !== deletedCartItem.id; 
     });
 
     //TODO Send delete item event to summary
+  }
+
+  incrementCount(updatedItem: CartItem) {
+    this.items = this.items.map(item => {
+      if (item.id === updatedItem.id)
+        item.count++;
+
+      return item;
+    });
+  }
+
+  decrementCount(item: CartItem) {
+    if (item.count > 0)
+      item.count--;
+
+    if (item.count === 0) {
+      this.deleteItem(item);
+    }
   }
 }
 
