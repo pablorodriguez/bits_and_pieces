27bca257395a4b61bc2476b521f4638d231d1411
Uriah Blatherwick <uriahb@gmail.com>
2023-11-14 14:20:16 -0600

    improve remove from cart to do single

--- cart-summary/src/CartSummary.svelte
+++ cart-summary/src/CartSummary.svelte
@@ -5,64 +5,82 @@
 
   let updateCategories = () => {
     categories = {};
-    items.forEach(item => {
-      if(!categories[item.category]){
+    items.forEach((item) => {
+      if (!categories[item.category]) {
         categories[item.category] = 0;
       }
       categories[item.category] += item.price;
     });
-  }
-  let addToCartHandler = (event) =>{
+  };
+  let addToCartHandler = (event) => {
     items.push(event.detail);
 
     count = items.length;
     updateCategories();
-  }
+  };
 
   let removeFromCartHandler = (event) => {
-    console.log(event.detail);
-    console.log(items);
-    const newItems = items.filter((prd) => {return prd.id != event.detail.id});
-    items = newItems;
-    console.log("items", items);
+    const firstIndex = items.findIndex(i => i.id == event.detail.id);
+    if (firstIndex >= 0) {
+      items.splice(firstIndex, 1);
+    }
     count = items.length;
     updateCategories();
-  }
+  };
 
   let clearCartHandler = () => {
     count = 0;
     items = [];
     categories = {};
-  }
+  };
 
-  window.addEventListener('addToCart', addToCartHandler);
-  window.addEventListener('removeFromCart', removeFromCartHandler);
-  window.addEventListener('clearCart', clearCartHandler);
-
+  window.addEventListener("addToCart", addToCartHandler);
+  window.addEventListener("removeFromCart", removeFromCartHandler);
+  window.addEventListener("clearCart", clearCartHandler);
 </script>
+
 <div class="container mb-5">
-      <p class="d-inline-flex gap-1">
-          <i class="bi bi-cart"></i>Cart ({count})
-      </p>
-      <a class="btn btn-link" data-bs-toggle="collapse" href="#cartSummary" role="button" aria-expanded="false" aria-controls="cartSummary">
-        Show Summary
-    </a>
-      <div class="collapse" id="cartSummary">
-          <div class="card card-body">
-            {#if Object.entries(categories) == 0}
-              <p>Nothing in your cart</p>
-            {/if}
-            {#each Object.entries(categories).sort() as [key, value], i}
-            <div>
-            {key} - ${value}
-            </div>
-            {/each}
-          </div>
-      </div>
+  <p class="d-inline-flex gap-1">
+    <i class="bi bi-cart" />Cart ({count})
+  </p>
+  <a
+    class="btn btn-link"
+    data-bs-toggle="collapse"
+    href="#cartSummary"
+    role="button"
+    aria-expanded="false"
+    aria-controls="cartSummary"
+  >
+    Show Summary
+  </a>
+  <div class="collapse" id="cartSummary">
+    <div class="card card-body">
+      {#if Object.entries(categories) == 0}
+        <p>Nothing in your cart</p>
+      {/if}
+      {#each Object.entries(categories).sort() as [key, value], i}
+        <div>
+          {key} - ${value}
+        </div>
+      {/each}
+    </div>
+  </div>
 </div>
 
 <svelte:head>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+    crossorigin="anonymous"
+  />
+  <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"
+  ></script>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"
+  />
 </svelte:head>
