eaeb9868c98101421a28969c56817d3f89ab47dd
Uriah Blatherwick <uriahb@gmail.com>
2023-11-14 09:51:11 -0600

    working? checkout component

--- checkout/webpack.config.js
+++ checkout/webpack.config.js
@@ -2,6 +2,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
+  output: {
+    path: __dirname + "/public",
+    filename: "[name].js",
+    chunkFilename: "[name].[id].js",
+    publicPath: "http://localhost:4200/",
+  },
   plugins: [
     new ModuleFederationPlugin({
       name: 'checkout',
@@ -9,9 +15,14 @@ module.exports = {
       exposes:{
         './CheckoutMain': './src/app/app.module.ts',
       },
+      shared: {
+        '@angular/core': {eager: true, singleton: true},
+        '@angular/common': {eager: true, singleton: true},
+        '@angular/router': {eager: true, singleton: true},
+      }
     }),
-    new HtmlWebpackPlugin({
-      template: './index.html'
-    })
+    // new HtmlWebpackPlugin({
+    //   template: './index.html'
+    // })
   ]
 };
